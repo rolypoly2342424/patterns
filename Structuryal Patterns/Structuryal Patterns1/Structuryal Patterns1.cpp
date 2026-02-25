c2715e9b2b3c3dff060b0952545f4e63ba778546
rolypoly <yapivapivpm@gmail.com>
2026-02-25 19:31:06 +0500

    создала шоколад и чай

--- Structuryal Patterns/Structuryal Patterns1/Structuryal Patterns1.cpp
+++ Structuryal Patterns/Structuryal Patterns1/Structuryal Patterns1.cpp
@@ -16,6 +16,15 @@ public:
     double cost() override { return 1.99; }
 };
 
+class Tea : public Beverage
+{
+public:
+    string getDescription() override { return "Чай"; }
+    double cost() override { return 1.50; }
+};
+
+
+
 class CondimentDecorator : public Beverage
 {
 protected:
@@ -50,6 +59,14 @@ public:
     double cost() override { return beverge->cost() + 0.15; }
 };
 
+class Chocolate : public CondimentDecorator
+{
+public:
+    Chocolate(Beverage* b) : CondimentDecorator(b) {}
+    string getDescription() override { return beverge->getDescription() + ", Шоколад"; }
+    double cost() override { return beverge->cost() + 0.20; }
+};
+
 
 
 int main()
@@ -59,15 +76,21 @@ int main()
     drink = new Milk(drink);
     drink = new Sugar(drink);
     drink = new Syrup(drink);
+    drink = new Chocolate(drink);
 
     cout << drink->getDescription() << " стоит $" << drink->cost() << endl;
 
     Beverage* simpleDrink = new Espresso();
     cout << simpleDrink->getDescription() << " стоит $" << simpleDrink->cost() << endl;
 
+    Beverage* tea1 = new Tea();
+    tea1 = new Milk(tea1);
+    tea1 = new Syrup(tea1);
+    cout << tea1->getDescription() << " стоит $" << tea1->cost() << endl;
+
     delete drink;
     delete simpleDrink;
-
+    delete tea1;
 
 
 }
